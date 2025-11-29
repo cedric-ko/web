8c48f3261f0b3955a6ab63b7dfd23f60913f3c1d
Alexey Utkin <cedrickobayashi@gmail.com>
2025-11-29 21:49:50 +0300

    Task 1 is done

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,53 @@
+package ru.netology.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldSendTheFormSuccessfully() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Сидоров Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79876543210");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button")).click();
+        WebElement text = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        assertTrue(text.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.getText().trim());
+    }
+}
